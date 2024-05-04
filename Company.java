87fce87862793cea3842c48374065a7938c8dc21
Michał Lenard <lenardmichal02@gmail.com>
2024-05-04 15:40:34 +0200

    zad4

--- Company.java
+++ Company.java
@@ -5,27 +5,26 @@ import employees.Worker;
 public class Company {
     
     public static void main(String[] args) {
-        Employee[] Employees = new Employee[5];
+        Employee[] Employees = new Employee[7];
         
-        Employees[0] = new Manager ("Michał Lenard", 20000, 0);
+        Employees[0] = new Manager ("Michał Lenard", 20000, 5);
         Employees[1] = new Worker("Jan Kowalski", 10000, "pracownik");
         Employees[2] = new Employee("Szymon Nowak", 5000);
         Employees[3] = new Employee("Piotr Kropka", 6000);
         Employees[4] = new Worker("Aneta Marzec", 7000, "pracownik");
-       
+        Employees[5] = new Manager ("Adam M", 10000, 5);
+        Employees[6] = new Worker("Karcper K", 9000, "pracownik");
 
-        //tą część kodu nie zrobiłem zamodzielnie
-        int pracownicy = 0;
-        for (Employee employee : Employees) {
-            if (!(employee instanceof Manager)) {
-            pracownicy++;
-            }
-        }   
-        ((Manager) Employees[0]).setNumberOfSubordinates(pracownicy);
-        //
-        Employees[0].setSalary(75000);
+        Employees[0].setSalary(20500);
+        Employees[1].setSalary(15000);
+        Employees[2].setSalary(5500);
+        Employees[3].setSalary(6500);
+        Employees[4].setSalary(7500);
+        Employees[5].setSalary(10500);
+        Employees[6].setSalary(9500);
 
-        
+        Employees[0].setSalary(7500);
+        Employees[6].setSalary(7500);
 
         System.out.println("Dane o wszystkich pracownikach:");
         System.out.println(Employees[0]);
@@ -33,6 +32,7 @@ public class Company {
         System.out.println(Employees[2]);
         System.out.println(Employees[3]);
         System.out.println(Employees[4]);
-       
+        System.out.println(Employees[5]);
+        System.out.println(Employees[6]);
     }
 }
